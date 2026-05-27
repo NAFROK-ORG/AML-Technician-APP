0c33d5f3c23884601a5374d71e5cc5dd111bcf5a
ahkhan-nafrok <a.h.khan555y@gmail.com>
2026-05-27 07:49:16 +0530

    Testing Rpo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -38,7 +38,7 @@ export default function Navbar() {
           fontSize: "13px", fontWeight: "700", color: "#fff",
           fontFamily: "'IBM Plex Mono', monospace",
         }}>
-          ML
+          AML
         </div>
         <span style={{ fontWeight: "700", fontSize: "15px", color: "var(--white)" }}>
           {isAdmin ? "Admin" : "Technician"}
